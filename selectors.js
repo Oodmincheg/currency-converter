5cf692f6b8524bdf642a4189a6b87a4aeb0bb336
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2022-08-31 20:09:04 +0300

    create selectores module.

---
+++ selectors.js
@@ -0,0 +1,5 @@
+export const fromSelect = document.querySelector('#from');
+export const toSelect = document.querySelector('#to');
+export const amountInput = document.querySelector('#amount');
+export const form = document.querySelector('form');
+export const resultDiv = document.querySelector('.result');
